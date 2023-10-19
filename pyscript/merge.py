860ac5b79bf96c479806ce93a60e750deac9db4c
val832 <valentin.naud@etudiant.univ-reims.fr>
2023-10-19 16:28:35 +0200

    nouvelles bdd

--- pyscript/merge.py
+++ pyscript/merge.py
@@ -8,7 +8,7 @@ from scrap import URLS, REGEX_PATTERN
 from tools import Crawler, Merge
 
 # préciser quelle bdd à enrichir en précisant l'index dans URLS
-index = 1
+index = 4
 headers = URLS[index]['url_headers']
 file_name = re.search(REGEX_PATTERN, URLS[index]['url']).group(1)
 "hello"
@@ -22,7 +22,7 @@ def merge_data(file_name):
     df1 = pd.read_csv(f"data/{file_name}.csv")
     sessions = [requests.Session() for _ in range(len(df1['lien']))]
     with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-        all_data = list(executor.map(Merge.fetch_data, df1['lien'].tolist(), [Na_table]*len(df1['lien']), sessions))
+        all_data = list(executor.map(Merge.fetch_data, df1['lien'], [Na_table]*len(df1['lien']), sessions))
     
     df2 = pd.DataFrame(all_data)
     df = pd.concat([df1, df2], axis=1)
