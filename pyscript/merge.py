003d4540a1ffb3074686a97c4a61a5a205273b4b
val832 <valentin.naud@etudiant.univ-reims.fr>
2023-10-19 12:18:06 +0200

    bdd bientôtexploitable

--- pyscript/merge.py
+++ pyscript/merge.py
@@ -2,84 +2,33 @@ import concurrent.futures
 from bs4 import BeautifulSoup
 import pandas as pd
 import re
+import requests
 
 from scrap import URLS, REGEX_PATTERN
-from tools import Crawler
+from tools import Crawler, Merge
 
 # préciser quelle bdd à enrichir en précisant l'index dans URLS
 index = 1
-url = URLS[index]
-file_name = re.search(REGEX_PATTERN, url).group(1)
+headers = URLS[index]['url_headers']
+file_name = re.search(REGEX_PATTERN, URLS[index]['url']).group(1)
 
-def missing_table (): 
-    res = Crawler.extract_html("https://www.harddrivebenchmark.net/hdd.php?hdd=35TTFP6PCIE-256G&id=27777")
-    table = Crawler.find_element(res, tag='table', element_id='test-suite-results')
-    print(table)
 
-    missing_table = {}
-    for row in table.findAll('tr'):
-        header = row.find('th').text
-        missing_table[header] = "NA"
     
-    return missing_table
-    
-Na_table = missing_table()
-
-def fetch_data(url):
-    res = Crawler.extract_html(url)
-    if not isinstance(res, BeautifulSoup):
-        print(f"Erreur : L'URL {url} n'a pas renvoyé un objet BeautifulSoup valide.")
-        return {'url': url}
-    
-    table = Crawler.find_element(res, tag='table', element_id='test-suite-results')
-    if table is None : 
-        print(f"bad {url}")
-    else : 
-        print(f"GOOD {url}")
-
-    try : 
-        data = {}
-        for row in table.findAll('tr'):
-            header = row.find('th').text
-            value = row.find('td').text
-            data[header] = value
-        return data 
-    except AttributeError : 
-        data = Na_table
-        return data 
+Na_table = Merge.missing_table(headers)
+print(Na_table)
 
 
-def merge_data(url, file_name):
 
-    html = Crawler.extract_html(url)
-    result = Crawler.find_element(html, tag='table', element_id='cputable')
-    if result is None : 
-        print(url)
-    body = result.find('tbody')
-    rows = body.find_all('tr')
-
-    base = re.search(r'(.*net/)', url).group(1)
-    urls = []
-
-    for row in rows:
-        a = row.find('a')
-        if a and a.get('href'):
-            urls.append(base + a.get('href'))
-
-    for index, current_url in enumerate(urls):
-        urls[index] = current_url.replace("_lookup", "")
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
-        all_data = list(executor.map(fetch_data, urls))
-
-    df2 = pd.DataFrame(all_data)
+def merge_data(file_name):
     df1 = pd.read_csv(f"data/{file_name}.csv")
-
+    sessions = [requests.Session() for _ in range(len(df1['lien']))]
+    with concurrent.futures.ThreadPoolExecutor(max_workers=50) as executor:
+        all_data = list(executor.map(Merge.fetch_data, df1['lien'].tolist(), [Na_table]*len(df1['lien']), sessions))
+    
+    df2 = pd.DataFrame(all_data)
     df = pd.concat([df1, df2], axis=1)
-    df.to_csv(f'data/{file_name}.csv')
-
+    df.to_csv(f'data/{file_name}2.csv')
     return df
 
-merge_data(url, file_name)
 
-"video_lookup"
+merge_data(file_name)
--- pyscript/tools.py
+++ pyscript/tools.py
@@ -308,3 +308,33 @@ class clean_df :
         else:
             return df.dropna()
         
+class Merge : 
+
+    def missing_table (url): 
+
+        res = Crawler.extract_html(url)
+        table = Crawler.find_element(res, tag='table', element_id='test-suite-results')
+
+        missing_table = {}
+        for row in table.findAll('tr'):
+            header = row.find('th').text
+            missing_table[header] = "NA"
+        
+        return missing_table
+    
+    def fetch_data(url, missing_table, session):
+        try :
+            res = Crawler.extract_html(url)
+            table = Crawler.find_element(res, tag='table', element_id='test-suite-results')
+            if table : 
+                print(f"good {url}")
+            data = {}
+            for row in table.findAll('tr'):
+                header = row.find('th').text
+                value = row.find('td').text
+                data[header] = value
+            return data 
+        except: 
+            print(url)
+            data = missing_table
+            return data 
