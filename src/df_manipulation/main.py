cbc9660333075929097abea8cb406db1d67c1809
val832 <valentin.naud@etudiant.univ-reims.fr>
2023-11-16 13:21:55 +0100

    fonctions + tests

--- src/df_manipulation/main.py
+++ src/df_manipulation/main.py
@@ -1,84 +1,8 @@
 import pandas as pd 
-import re 
+from df_tools import count_amenities
 
 df_rbnb = pd.read_csv('data/listings.csv', sep = ',')
 
-#Permet de donner un aperçu de la qualité des données. 
-print(df_rbnb.info())
+df_amenities = count_amenities(df_rbnb)
 
-distinct_amenities = []
-
-for amenities in df_rbnb['amenities'] : 
-
-    # Convertir la chaîne en une liste Python réelle
-    import ast
-    liste_reelle = ast.literal_eval(amenities)
-
-    # Étape 1: Retirer les guillemets existants
-    liste_sans_guillemets = [element.replace('"', '') for element in liste_reelle]
-
-    # Étape 2: Joindre les éléments en une chaîne, séparés par ', '
-    chaine = ', '.join(liste_sans_guillemets)
-
-    # Étape 3: Ajouter des guillemets au début et à la fin
-    chaine_avec_guillemets = f'"{chaine}"'
-    
-    # Étape 4: Convertir la chaîne en liste
-    liste_finale = chaine_avec_guillemets.split(', ')
-    if liste_finale[0][0] == '"':
-        liste_finale[0] = liste_finale[0][1:]
-    if liste_finale[-1][-1] == '"':
-        liste_finale[-1] = liste_finale[-1][:-1]
-    
-    for ameniti in liste_finale :
-        
-        if ameniti not in distinct_amenities : 
-            
-            distinct_amenities.append(ameniti)
-
-
-
-import ast
-from collections import defaultdict
-
-# Initialisation d'un dictionnaire pour compter les occurrences de chaque amenity
-distinct_amenities = defaultdict(int)
-
-for amenities in df_rbnb['amenities']:
-    # Convertir la chaîne en liste
-    liste_reelle = ast.literal_eval(amenities)
-
-    # Retirer les guillemets et nettoyer les données
-    liste_sans_guillemets = [element.replace('"', '') for element in liste_reelle]
-    chaine = ', '.join(liste_sans_guillemets)
-    chaine_avec_guillemets = f'"{chaine}"'
-    liste_finale = chaine_avec_guillemets.split(', ')
-    
-    if liste_finale[0][0] == '"':
-        liste_finale[0] = liste_finale[0][1:]
-    if liste_finale[-1][-1] == '"':
-        liste_finale[-1] = liste_finale[-1][:-1]
-
-    # Mise à jour du compteur pour chaque amenity
-    for amenity in liste_finale:
-        distinct_amenities[amenity] += 1
-
-# 1. Transformer le dictionnaire en liste de tuples et 2. Trier la liste
-sorted_amenities = sorted(distinct_amenities.items(), key=lambda x: x[1], reverse=True)
-
-# 3. Créer un DataFrame
-df_amenities = pd.DataFrame(sorted_amenities, columns=['Amenity', 'Frequency'])
-
-# 4. Enregistrer le DataFrame dans un fichier CSV
 df_amenities.to_csv('data/amenities_frequency.csv', index=False, encoding="utf-8", errors='replace')
-
-
-
-
-
-# Chemin du fichier où enregistrer la liste
-"""file_path = 'utils/mod.txt'
-
-with open(file_path, 'w', encoding='utf-8', errors='replace') as file:
-    for item in distinct_amenities:
-        file.write("%s\n" % item)"""
--- tests/test_df_tools.py
+++ tests/test_df_tools.py
@@ -1,22 +1,25 @@
 import pandas as pd 
 import unittest
-from src.df_manipulation.df_tools import create_column_from_match
+from src.df_manipulation.df_tools import *
 
 class TestCreateColumnFromMatch(unittest.TestCase):
 
     def setUp(self):
+        # Initialize a DataFrame with sample data for testing
         self.df = pd.DataFrame({'text': ['Hello World', 'hello universe', 
                                          'goodbye world', 'world with Jacuzzi']})
     
     def test_single_word(self):
+        # Test creating a column based on a single word match
         result_df = create_column_from_match(self.df, 'text', word='hello')
-        self.assertIn('hello', result_df.columns)
-        self.assertListEqual(list(result_df['hello']), [1, 1, 0, 0])
-    
+        self.assertIn('hello', result_df.columns)  # Check if 'hello' column is created
+        self.assertListEqual(list(result_df['hello']), [1, 1, 0, 0])  # Verify the column values
+
     def test_words_list(self):
+        # Test creating multiple columns based on a list of words
         result_df = create_column_from_match(self.df, 'text', 
                                              words_list=['hello', 'world', 'jacuzzi'])
-    
+        # Check for each word if the corresponding column is created and verify its values
         for word in ['hello', 'world', 'jacuzzi']:
             self.assertIn(word, result_df.columns)
         self.assertListEqual(list(result_df['hello']), [1, 1, 0, 0])
@@ -24,33 +27,72 @@ class TestCreateColumnFromMatch(unittest.TestCase):
         self.assertListEqual(list(result_df['jacuzzi']), [0, 0, 0, 1])
     
     def test_invalid_df(self):
+        # Test the function with invalid DataFrame input
         with self.assertRaises(TypeError):
             create_column_from_match("not a dataframe", 'text', word='hello')
     
     def test_invalid_reference_column(self):
+        # Test the function with a non-existent column name
         with self.assertRaises(ValueError):
             create_column_from_match(self.df, 'non_existent_column', word='hello')
     
     def test_no_word_or_list(self):
+        # Test the function without providing a word or a list of words
         with self.assertRaises(ValueError):
             create_column_from_match(self.df, 'text')
     
     def test_both_word_and_list(self):
+        # Test the function when both a single word and a list of words are provided
         with self.assertRaises(ValueError):
             create_column_from_match(self.df, 'text', word='hello', words_list=['world'])
     
     def test_invalid_word_type(self):
+        # Test the function with a non-string type for the word parameter
         with self.assertRaises(TypeError):
             create_column_from_match(self.df, 'text', word=123)
     
     def test_invalid_words_list_type(self):
+        # Test the function with an invalid type for the words_list parameter
         with self.assertRaises(TypeError):
             create_column_from_match(self.df, 'text', words_list='hello')
     
     def test_invalid_element_in_words_list(self):
+        # Test the function with a non-string element in the words list
         with self.assertRaises(TypeError):
             create_column_from_match(self.df, 'text', words_list=['hello', 123])
-    
+
+
+class TestCountAmenities(unittest.TestCase):
+    def setUp(self):
+        # Set up sample data for testing
+        self.example_amenities = [
+            '["Microwave", "Wifi", "Iron"]',
+            '["Iron", "Air conditioning", "Wifi"]',
+            '["Wifi", "Heating"]'
+        ]
+        # Create a DataFrame from the sample data
+        self.df_example = pd.DataFrame({'amenities': self.example_amenities})
+
+    def test_basic_count(self):
+        # Test if the function correctly counts the frequency of 'Wifi'
+        result = count_amenities(self.df_example)
+        self.assertEqual(result.loc[result['Amenity'] == 'Wifi', 'Frequency'].values[0], 3)
+
+    def test_data_cleaning(self):
+        # Test if the function properly cleans the data (removes quotes)
+        result = count_amenities(self.df_example)
+        self.assertNotIn('"', result['Amenity'].values)
+
+    def test_sorting(self):
+        # Test if the amenities are sorted correctly in descending order of frequency
+        result = count_amenities(self.df_example)
+        self.assertTrue(result.iloc[0]['Frequency'] >= result.iloc[-1]['Frequency'])
+
+    def test_output_format(self):
+        # Test if the output DataFrame is formatted correctly (correct column names)
+        result = count_amenities(self.df_example)
+        self.assertTrue(set(result.columns) == {'Amenity', 'Frequency'})
+
 if __name__ == '__main__':
     unittest.main()
 
