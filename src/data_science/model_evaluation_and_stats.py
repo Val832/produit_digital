83df6f57a03872fd5e5b645a72a1af316b37b0c4
Garance Gabaut <garance.gabaut@etudiant.univ-reims.fr>
2023-12-05 07:50:07 +0100

    mise à jour du formulaire en fonction des dernières variables choisies dans le modèle

--- src/data_science/model_evaluation_and_stats.py
+++ src/data_science/model_evaluation_and_stats.py
@@ -1,22 +1,21 @@
-#import csv_excel_operation
-# from 
 import csv_excel_operations as ceo
 import os 
 
+# keep the current path
 current_directory = os.getcwd()
-# Remonter de deux niveaux dans la hiérarchie du chemin
+
+# keep the beggining of the path
 parent_directory = os.path.dirname(os.path.dirname(current_directory))
 
 # Define file paths and constants
 file_path_data = os.path.join(parent_directory, 'VBA\data.csv')
 file_path_estimation= os.path.join(parent_directory, 'VBA\estimation.csv')
 
-cell_reference = 'A1'
-sheet_name = 'estimation'
+cell_reference = 'B1'
+sheet_name = 'Traitement'
 
 
 data = ceo.read_csv(file_path_data)
-
 count = ceo.count_false_elements(data)
 
 
